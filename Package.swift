cb6fee8618c0c033b7230d6c55c92c000d121e19
Sofia <sofia.chevrolat@madimedia.pro>
2023-09-24 07:47:31 -0400

    feat: Initial commit 🐿️
    Set up the Package.swift file with Squirrel target and test target

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Squirrel",
+    platforms: [
+      .iOS(.v16),
+      .macOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "Squirrel",
+            targets: [
+                "Squirrel"
+            ]
+        ),
+    ]
+)
+
+// MARK: - Squirrel
+
+package.targets.append(contentsOf: [
+    Target
+        .target(
+            name: "Squirrel",
+            dependencies: [
+            ],
+            path: "Sources/Squirrel"
+        ),
+        .testTarget(
+            name: "SquirrelTests",
+            dependencies: [
+                "Squirrel"
+            ],
+            path: "Tests/SquirrelTests"
+        ),
+    ]
+)
